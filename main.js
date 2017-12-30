9c7b57e7224c9e0a2c6e469fef3d712bef6e6ca8
Sima Margulis Davidi <simamarg@gmail.com>
2017-12-30 23:35:46 +0200

    add and remove spacebook comments + ui facelift

--- main.js
+++ main.js
@@ -2,63 +2,96 @@ var posts = [];
 var postId = 0;
 var commentId = 0;
 
+// database functions
 function addPost(text) {
     postId++;
     var post = {text: text, id: postId, comments: []};
     posts.push(post);
 }
 
-function renderPosts() {
-    $('.posts').find('p').remove();
+function findElementIndex(array, elementId) {
+    return array.findIndex(function(element) {
+        return element.id === elementId;
+    });
+}
+
+function removePost(id) {
+    posts.splice(findElementIndex(posts, id), 1);
+}
+
+function addComment(commentName, commentText, postId) {
+    commentId++;
+    var comment = {name: commentName, comment: commentText, id: commentId};
+    posts[findElementIndex(posts, postId)].comments.push(comment);
+}
+
+function removeComment(postId, commentId) {
+    var postIndex = findElementIndex(posts, postId);
+    var postComments = posts[postIndex].comments;
+    var commentIndex = findElementIndex(postComments, commentId);
+    postComments.splice(commentIndex, 1);
+}
+
+// view functions
+function renderPosts() {    
+    $('.posts').empty();
     for (var i=0; i < posts.length; i++) {
-        var currPost = posts[i];
-        $('.posts').append('<p class="post" data-index="' + i + '" data-id="' + currPost.id + 
-        '"> ' + currPost.text + ' <a class="comment" href="#">Comment</a> ' + 
-        '<button class="btn btn-danger remove" type="button">REMOVE</button> </p>');
+        var post = posts[i];
+        var html = `<div class="post" data-id="${post.id}">
+                        <div class="post-body">
+                            <p class="post-text">${post.text}</p>
+                            <div class="post-link remove-post">Remove</div>
+                            <div class="post-link add-comment">Comment</div>
+                            <ul class="list-group comments"> ${renderComments(post.comments)} </ul>
+                        </div> 
+                    </div>`;
+        $('.posts').append(html);
     }
 }
 
-function removePost(postIndex) {
-    posts.splice(postIndex, 1);
+function renderComments(comments) {
+    var html = "";
+    for (var i=0; i < comments.length; i++) {
+        var comment = comments[i];
+        html += `<li class="list-group-item comment" data-id="${comment.id}">
+                    <p> <span class="name">${comment.name}</span>${comment.comment}</p>
+                    <div class="post-link remove-comment">Remove</div>
+                </li>`;
+    }
+    return html;
 }
 
-function addComment(commentText, postIndex) {
-    commentId++;
-    var comment = {comment: commentText, id: commentId};
-    posts[postIndex].comments.push(comment);
-}
-
-// TODO write renderComments function
-function renderComments() {
-    $('.posts').find('.comment-form p').remove();
-//     for (var i=0; i < posts.length; i++) {
-//         var currPost = posts[i];
-//         $('.posts').append('<p class="post" data-index="' + i + '" data-id="' + currPost.id + 
-//         '"> <button class="btn btn-danger remove" type="button">REMOVE</button> ' + currPost.text + 
-//         ' <a class="comment" href="#">Comment</a> </p>');
-//     }
-}
-
+// on-click functions
 $('button.add-post').click(function() {
     addPost($('#post-name').val());
     renderPosts();
     $('#post-name').val('');
 });
 
-$('.posts').on('click', 'button.remove', (function() {
-    removePost($(this).closest('p').data().index);
+$('.posts').on('click', 'div.remove-post', function() {
+    removePost($(this).closest('div.post').data().id);
     renderPosts();
-}));
-
-$('.posts').on('click', 'a.comment', (function() {
-    $(this).closest('p').append('<div class="comment-form"> <div class="form-group">' + '<h4>Add a Comment</h4>' + 
-    '<input type="text" id="comment-name" class="form-control" placeholder="Comment Text, User Name"> </div>' + 
-    '<button class="btn btn-primary add-comment" type="button">Post comment</button>' + '</div>');
-}));
-
-$('.posts').on('click', 'button.add-comment', function() {
-    var currPost = $(this).closest('p');
-    addComment(currPost.find('input#comment-name').val(), currPost.data().index);
-    // renderComments();
 });
 
+$('.posts').on('click', 'div.add-comment', (function() {
+    $(this).closest('div.post-body').append(`<div class="write-comment">
+                                                <input type="text" class="form-control comment-name" placeholder="What's your name?">
+                                                <input type="text" class="form-control comment-text" placeholder="Write a comment...">
+                                                <button class="btn btn-primary" type="button">Post</button>
+                                            </div>`);
+}));
+
+$('.posts').on('click', '.write-comment button', function() {
+    var name = $(this).closest('div.write-comment').find('.comment-name').val();
+    var text = $(this).closest('.post-body').find('.comment-text').val();
+    var id = $(this).closest('.post').data().id;
+    addComment(name, text, id);
+    renderPosts();
+});
+
+$('.posts').on('click', 'div.remove-comment', function() {
+    var postId = $(this).closest('.post').data().id;
+    var commentId = $(this).parent().data().id;
+    removeComment(postId, commentId);
+    renderPosts();
+});
