db45d7bf45f1a8bbd36ce8b75b28d2681b0fda82
Sima Margulis Davidi <simamarg@gmail.com>
2018-01-01 22:45:48 +0200

    create comment functions

--- main.js
+++ main.js
@@ -32,7 +32,8 @@ var SpacebookApp = function () {
   var createPost = function (text) {
     var post = {
       text: text,
-      id: currentId
+      id: currentId,
+      comments: []
     }
 
     currentId += 1;
@@ -59,9 +60,7 @@ var SpacebookApp = function () {
   var removePost = function (currentPost) {
     var $clickedPost = $(currentPost).closest('.post');
     var id = $clickedPost.data().id;
-
     var post = _findPostById(id);
-
     posts.splice(posts.indexOf(post), 1);
     $clickedPost.remove();
   }
@@ -71,19 +70,49 @@ var SpacebookApp = function () {
     $clickedPost.find('.comments-container').toggleClass('show');
   }
 
+  var createComment = function (currentPost) {
+    var $clickedPost = $(currentPost).closest('.post');
+    var postId = $clickedPost.data().id;
+    var post = _findPostById(postId);
+
+    var commentText = $clickedPost.find('.comment-name').val();
+    var comment = {text: commentText};
+    posts[posts.indexOf(post)].comments.push(comment);
+  }
+
+  var renderComments = function () {
+    this.renderPosts();
+    for (i = 0; i < posts.length; i += 1) {
+      var commentList = '';
+      var postComments = posts[i].comments;
+      for (j = 0; j < postComments.length; j += 1) {
+        commentList += '<li data-id="' + j + '">' + postComments[j].text + 
+        '<a href="#" class="remove-comment">remove</a> </li>';
+      }
+      commentList = '<ul>' + commentList + '</ul>';
+      var currentPost = '.post[data-id=' + posts[i].id + ']'; 
+      $(currentPost).append(commentList);
+    }
+  }
+
+  var removeComment = function (currentPost) {
+    var $clickedPost = $(currentPost).closest('.post');
+    var id = $clickedPost.data().id;
+    var post = _findPostById(id);
+    var commentIndex = $(currentPost).closest('li').data().id;
+    posts[posts.indexOf(post)].comments.splice(commentIndex, 1);
+  }
+
   return {
+    posts: posts,
     createPost: createPost,
     renderPosts: renderPosts,
     removePost: removePost,
+    createComment: createComment,
+    renderComments: renderComments,
 
     // TODO: Implement
-    // createComment: createComment,
-
-    // TODO: Implement
-    // renderComments: renderComments,
-
-    // TODO: Implement
-    // removeComment: removeComment,
+    removeComment: removeComment,
     toggleComments: toggleComments
   }
 }
@@ -99,6 +128,7 @@ $('.add-post').on('click', function () {
   
   app.createPost(text);
   app.renderPosts();
+  app.renderComments();
 });
 
 $('.posts').on('click', '.remove', function () {
@@ -107,4 +137,15 @@ $('.posts').on('click', '.remove', function () {
 
 $('.posts').on('click','.show-comments', function () {
   app.toggleComments(this);
+});
+
+$('.posts').on('click', '.add-comment', function () {
+  app.toggleComments(this);
+  app.createComment(this);
+  app.renderComments();
+});
+
+$('.posts').on('click', '.remove-comment', function () {
+  app.removeComment(this);
+  app.renderComments();
 });
